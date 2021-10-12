fd1d8aa74fe4ad1a57155420a338f1d709914173
Brian Matheson <bmath@mayadata.io>
2021-10-12 13:47:26 -0400

    add pod name to output

--- src/main/java/com/vmware/springboozle2/GreetingController.java
+++ src/main/java/com/vmware/springboozle2/GreetingController.java
@@ -8,11 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
-  private static final String template = "Hello, %s!";
+  private static final String template = "Hello, %s, from k8s pod %s!";
   private final AtomicLong counter = new AtomicLong();
 
   @GetMapping("/greeting")
   public Greeting greeting(@RequestParam(value = "name", defaultValue = "World")  String name){
-    return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    String value = System.getenv("HOSTNAME");
+    return new Greeting(counter.incrementAndGet(), String.format(template, name, value));
   }
 }
